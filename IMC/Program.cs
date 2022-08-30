d031a712b034cc6e59e3dd3bc6b394a569a86ef5
meduardaalve <meduardaalve@BF-PE064AES>
2022-08-29 21:33:55 -0300

    Adicionar arquivos de projeto.

---
+++ IMC/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso;
+            float altura;
+            float resultado;
+            
+          
+
+            Console.Write("Entre com a sua idade \n");
+            Console.ReadLine(); 
+
+            Console.Write("Entre com o seu peso \n");
+            peso = float.Parse(Console.ReadLine());
+           
+            
+
+            Console.Write("Entre com a sua altura \n");
+            altura = float.Parse(Console.ReadLine());
+            
+            
+
+            resultado = (peso /(altura*altura ));
+            
+
+            if (resultado<18) 
+
+
+            Console.Write("Você está abaixo do peso");
+            Console.ReadLine();
+
+            if (resultado <= 24)
+                Console.Write("Seu peso está normal");
+                Console.ReadLine();
+
+            if (resultado <= 29)
+                Console.Write("Você está com sobrepeso.");
+                Console.ReadLine();
+
+            if (resultado <= 34)
+                Console.Write("Você está com obesidade grau I");
+                Console.ReadLine(); 
+            if (resultado <= 39)
+                Console.Write("Você está com obesidade grau II");
+                Console.ReadLine();
+
+            if (resultado <= 34)
+                Console.Write("Você está com obesidade grau III");
+                Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
